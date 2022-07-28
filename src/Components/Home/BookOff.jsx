1c428ac5c75530e486acee9ab042eb2043886616
Imam Abubakar <54324954+Imam-Abubakar@users.noreply.github.com>
2022-07-28 12:36:25 +0100

    july update

---
+++ src/Components/Home/BookOff.jsx
@@ -0,0 +1,46 @@
+import { VStack, Heading, Box } from '@chakra-ui/react';
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import ArrowButton from '../HOC/ArrowButton';
+
+const BookNow = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    if (pathname === '/') {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    } else {
+      navigate('/');
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  return (
+    <VStack
+      alignItems={'start'}
+      w='fit-content'
+      spacing={{ base: '1rem', md: '2rem' }}
+      mx={{ base: '2rem', md: 'auto' }}
+      my='1rem'
+    >
+      <Heading
+        fontWeight={'500'}
+        fontSize={{ base: 'xl', md: '4xl' }}
+        maxW='36rem'
+      >
+        GET 20% OFF YOUR FIRST ORDER
+      </Heading>
+      <Box as='button' onClick={onClickHandler}>
+        <ArrowButton variant='dark'>Book Now</ArrowButton>
+      </Box>
+    </VStack>
+  );
+};
+
+export default BookNow;
